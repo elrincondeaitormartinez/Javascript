d58e95eac25b545fcc51cc4503a7966f33d4be32
Aitor Martínez Rodríguez <56599232+elrincondeaitormartinez@users.noreply.github.com>
2020-01-16 09:33:35 +0100

    Add files via upload

---
+++ Tarefa5_ducias2ovos/script.js
@@ -0,0 +1,19 @@
+var ducias;
+ducias = prompt("Cuantas docenas de huevos lleva tu tortilla?     ");
+
+var numero;
+numero=parseInt(ducias);
+alert(numero);
+var cuenta;
+cuenta=numero*12
+alert("Tu tortilla tiene " + cuenta + " huevos")
+
+//var resposta;
+//resposta = prompt("Cuantos euros quieres convertir?     ");
+
+//var numero;
+//numero=parseInt(resposta);
+//alert(numero);
+//var cuenta;
+//cuenta=numero*122.51
+//alert(numero +" euro/s son " + cuenta + " yenes")
